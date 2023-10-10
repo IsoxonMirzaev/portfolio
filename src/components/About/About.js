4ab52c346e513005f7a97cd636d4768f2e0b5de6
IsoxonMirzaev <isoxonmirzayev551@gmail.com>
2023-10-10 15:53:06 +0500

    yangilandi

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,20 +6,13 @@ function About() {
       <div name="about" className="h-screen">
         <h1 className="text-center mb-10 text-5xl font text-white">About Me</h1>
         <p className="w-2/3 h-40 text-center m-auto text-white text-xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-          aspernatur suscipit laudantium, ad aliquid est deleniti autem sint
-          explicabo libero blanditiis magnam error quod fugit maiores ut ipsam
-          ea mollitia? Est praesentium molestiae aliquam pariatur animi cumque,
-          temporibus minus consequuntur nulla repellendus debitis quo eos,
-          aperiam culpa. Repellat, facilis fugit.
+          I am Isokhan Mirzayev. I am 12 years old. I started web programming in
+          4th grade. I currently know Html, Css, Javascript, Tailwind, Sass,
+          React, and NextJS.
         </p>
-        <p className="w-2/3 h-40 text-center m-auto text-white text-xl mt-44 md:mt-10">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut
-          aspernatur suscipit laudantium, ad aliquid est deleniti autem sint
-          explicabo libero blanditiis magnam error quod fugit maiores ut ipsam
-          ea mollitia? Est praesentium molestiae aliquam pariatur animi cumque,
-          temporibus minus consequuntur nulla repellendus debitis quo eos,
-          aperiam culpa. Repellat, facilis fugit.
+        <p className="w-2/3 h-40 text-center m-auto text-white text-xl mt-10">
+          My address is Namangan city of Uzbekistan, Obod neighborhood, 1 house.
+          My phone number is +998 90 752 95 02
         </p>
       </div>
     </>
